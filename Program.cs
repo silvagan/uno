470154510392979c523a6f5aa9c86e40c774b63c
Silva <krikon1@ktu.lt>
2024-07-26 20:13:51 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Raylib_CsLo;
+
+namespace Uno;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Raylib.InitWindow(1280, 720, "Hello, Raylib-CsLo");
+        Raylib.SetTargetFPS(60);
+        // Main game loop
+        while (!Raylib.WindowShouldClose()) // Detect window close button or ESC key
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Raylib.SKYBLUE);
+            Raylib.DrawFPS(10, 10);
+            Raylib.DrawText("networking is hard D:!!!", 640, 360, 50, Raylib.RED);
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+}
